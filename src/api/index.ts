48380733806dee229213b412d376835a02545fac
bachbone <gioperalt030412@gmail.com>
2025-07-26 14:54:38 +0800

    try deploy

---
+++ src/api/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import start from '../index';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_, res) => {
+
+  start();
+
+  res.send('Etheria United Front Bot API is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
